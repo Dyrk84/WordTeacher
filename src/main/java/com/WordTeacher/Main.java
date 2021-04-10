0db9d037800f9fb7441c1a7e6f41afbfd0e5666a
David Diriczi <diriczid@hotmail.com>
2021-04-10 19:07:51 +0200

    after first commit

---
+++ src/main/java/com/WordTeacher/Main.java
@@ -0,0 +1,14 @@
+package com.WordTeacher;
+
+import com.WordTeacher.tools.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.menu();
+
+    }
+
+
+
+}
---
+++ src/main/java/com/WordTeacher/tools/Menu.java
@@ -0,0 +1,63 @@
+package com.WordTeacher.tools;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static com.WordTeacher.utils.Colors.RED;
+import static com.WordTeacher.utils.Colors.RESET;
+
+public class Menu {
+    //adja meg az angol szót:
+    //adja meg a magyar jelentését:
+    //felírja a listákba az inputot
+    WordTeacher wt = new WordTeacher();
+
+    public void menu() {
+        int chosenNumber = chosenNumber();
+        switch (chosenNumber) {
+            case 1:
+                wt.enteringAWordToLearn();
+                break;
+            case 2:
+                wt.InterrogationOfWords();
+                break;
+            case 3:
+                exit();
+                break;
+            default:
+                yourChooseIsNotAppropriate();
+                menu();
+        }
+    }
+
+    private int chosenNumber() {
+        do {
+            printMenu();
+            Scanner scanner = new Scanner(System.in);
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException hibafogo) {
+                yourChooseIsNotAppropriate();
+            }
+        } while (true);
+    }
+
+    private void printMenu() {
+        System.out.println("*********************** Menu *****************************");
+        System.out.println("*" + RED.typeOfColor + " Choose one of the following possibilities:            " + RESET.typeOfColor +
+                " *");
+        System.out.println("* " + RED.typeOfColor + "1." + RESET.typeOfColor + " Entering a word to learn                            *");
+        System.out.println("* " + RED.typeOfColor + "2." + RESET.typeOfColor + " Interrogation of words                              *");
+        System.out.println("* " + RED.typeOfColor + "3." + RESET.typeOfColor + " Exit                                                *");
+        System.out.println("**********************************************************");
+    }
+
+    private void yourChooseIsNotAppropriate() {
+        System.out.println(RED.typeOfColor + "Your choose is not appropriate!" + RESET.typeOfColor);
+    }
+
+    public void exit() {
+        System.out.println("Good bye!");
+    }
+
+}
